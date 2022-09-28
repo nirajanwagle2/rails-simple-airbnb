ee93522ae71a8f6996450e7776dab5331f56cf63
nirajan <nirajanwagle2@gmial.com>
2022-09-28 14:58:18 +0200

    Update method improved

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -10,12 +10,24 @@ class FlatsController < ApplicationController
   def new
   end
 
+  def edit
+    @flat = Flat.find(params[:id])
+  end
   def create
   end
 
   def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
   end
 
   def destroy
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :flats, only: [:index, :show, :new, :create, :update, :destroy]
+  resources :flats, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
